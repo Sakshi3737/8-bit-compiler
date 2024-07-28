f6129f8646247fef1cacd046e4be858ec0453281
Sakshi3737 <134384945+Sakshi3737@users.noreply.github.com>
2024-07-28 16:33:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-// (c) 2018 Max Apodaca
-// This code is licensed under MIT license (see LICENSE for details)
+import Computer from "./Computer.js";
+import Editor from "./Editor.js";
+import Visualizer from "./Visualizer.js";
 
-var fs = require('fs');
-var parser = require('./Parser.js');
-var serialPortWriter = require('./SerialPortWriter.js');
-
-if(process.argv.length < 4){
-  console.log("ERROR: TOO FEW ARGUMENTS");
-  return;
-}
-
-var data = fs.readFileSync(process.argv[2], 'utf-8');
-
-let assembly = parser(data);
-console.log(assembly.toString());
-let bytes = assembly.getBytes();
-
-for(let i = 0; i<bytes.length; i+=2){
-  console.log((i/2) + ": " + bytes[i].toString(2) + " " + bytes[i+1].toString(2));
-}
-console.log();
-
-console.log(assembly.toString());
-
-fs.writeFileSync(process.argv[3],new Buffer(bytes));
-
-if(process.argv.length > 4){
-  serialPortWriter(process.argv[4], new Buffer(bytes));
-}
+window.computer = new Computer();
+window.editor = new Editor(window.computer);
+window.visualizer = new Visualizer(window.computer);
